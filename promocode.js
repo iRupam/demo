e4bca4d6bdf7ca2cc19c38399724bf084807adce
Rupam Medhi <27852445+iRupam@users.noreply.github.com>
2019-02-21 20:42:56 +0530

    Uploaded promocode files

---
+++ promocode.js
@@ -0,0 +1,15 @@
+
+// itemPurchase
+
+var itemPurchased = window.prompt('Please tell us the no of items purchased?')
+
+if(itemPurchased>3){
+	alert('You have got a promo code')
+} else if(itemPurchased == 3) {
+ alert('purchase one more item and you will get promo code')
+}else{
+alert('You are not eligible for a promo code!!')
+}
+
+//end item Purchase
+	
